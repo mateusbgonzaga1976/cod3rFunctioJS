514f79692857e1743b9920262d2662de32ec9ef7
mateusbgonzaga1976 <mateusbgonzaga@gmail.com>
2023-01-28 14:19:27 -0300

    Exercise 10 - JS list cod3r course

---
+++ exerc10.js
@@ -0,0 +1,13 @@
+function divideBy3(number){
+    if(number % 3 == 0){
+        console.log(`The number ${number} is divide by 3.`);
+    } else {
+        console.log(`The number ${number} is not divide by 3.`);
+    }
+}
+
+divideBy3(0);
+divideBy3(1);
+divideBy3(3);
+divideBy3(8);
+divideBy3(9);
