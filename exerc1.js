685e3fab8e1bb3c0a225da5dc4e7ba99f60ebdbc
mateusbgonzaga1976 <mateusbgonzaga@gmail.com>
2023-01-28 00:54:19 -0300

    Exercicio 1 de funcoes da lista do curso cod3r

---
+++ exerc1.js
@@ -0,0 +1,11 @@
+function aritmetic(num1, num2){
+    console.log(num1 + num2,
+                num1 - num2,
+                num1 * num2,
+                (num1 / num2).toFixed(2),
+                num1 % num2,
+                num1 ** num2,
+                num1 * 1/num2);
+}
+
+aritmetic(25, 5);
